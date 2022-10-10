ca6797ec83456de0388aa51067f7c9e265498b69
Jitesh Bhashwani <jitubhashwani999@gmail.com>
2022-10-10 07:51:03 +0530

    getMinMax gfg

--- DSA-Practice/LOVEBABBAR-DSA-SHEET/Array/Find the maximum and minimum_element_in_an_array.cpp
+++ DSA-Practice/LOVEBABBAR-DSA-SHEET/Array/Find the maximum and minimum_element_in_an_array.cpp
@@ -1,48 +1,36 @@
 //{ Driver Code Starts
-/* Driver program to test above function */
-
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-// } Driver Code Ends
-// User function Template for C++
+pair<long long, long long> getMinMax(long long a[], int n) ;
 
-class Solution
-{
-public:
-    int findSum(int A[], int N)
-    {
-        // code here.
-        int min{A[0]}, max{A[0]};
-        for (int i = 0; i < N; i++)
-        {
-            if (A[i] > max)
-                max = A[i];
-            if (A[i] < min)
-                min = A[i];
-        }
-        return min + max;
-    }
-};
-
-//{ Driver Code Starts.
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n;
         cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-        Solution ob;
-        int ans = ob.findSum(arr, n);
-        cout << ans;
-        cout << "\n";
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
     }
     return 0;
 }
-
 // } Driver Code Ends
+
+// solution =====================================================
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    auto answer = make_pair<long long,long long>(INT_MAX,INT_MIN);
+    for (size_t i = 0; i < n; i++)
+    {
+        answer.first = min(answer.first,a[i]);
+        answer.second = max(answer.second,a[i]);
+    }
+    return answer;
+}
+//===============================================================
