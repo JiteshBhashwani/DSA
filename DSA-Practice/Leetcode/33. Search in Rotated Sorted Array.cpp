56efeac87841982ff54e2eb092abc9b0ce33746e
Jitesh Bhashwani <jitubhashwani999@gmail.com>
2022-11-01 07:38:41 +0530

    leetcode q33 optimized

--- DSA-Practice/Leetcode/33. Search in Rotated Sorted Array.cpp
+++ DSA-Practice/Leetcode/33. Search in Rotated Sorted Array.cpp
@@ -3,37 +3,30 @@ using namespace std;
 
 class Solution {
 public:
-int search(vector<int>& nums, int target) {
-        int pev;
-        if(nums[0] > nums[nums.size()-1])
+    int findPivot(vector<int>& arr)
+    {
+        int f = 0;
+        int l = arr.size()-1;
+        while(f<l)
         {
-            int f{0};unsigned long l{nums.size()-1};
-            while(f<l)
-            {
-                int mid = (f+l)/2;
-                if(nums[mid] < nums[l]) l = mid;
-                else f = mid+1;
-            }
-            pev = l;
-            int f1{0};int l1{pev};
-            while(f1 <= l1)
-            {
-                int mid = (f1+l1)/2;
-                if(nums[mid] == target) return mid;
-                else if(nums[mid] < target) f1 = mid+1;
-                else l1 = mid-1;
-            }
+            int mid = (f+l)/2;
+            if(arr[mid] < arr[0]) l = mid;
+            else f = mid+1;
         }
-        else pev = 0;
-        int f2{pev};
-        int l2 = nums.size()-1;
-        while(f2<=l2)
-        {
-            int mid = (f2+l2)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) f2 = mid+1;
-            else if(nums[mid] > target) l2 = mid-1;
-        }
-        return -1;
+        return f;
+    }
+    int binarySearch(vector<int>& arr,int k,int f,int l)
+    {
+        if(f>l) return -1;
+        int mid = (f+l)/2;
+        if(arr[mid] < k) return binarySearch(arr,k,mid+1,l);
+        else if(arr[mid] > k) return binarySearch(arr,k,f,mid-1);
+        return mid;
+    }
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int pivot = findPivot(nums);
+        if(nums[pivot] <= target && target <= nums[n-1]) return binarySearch(nums,target,pivot,n-1);
+        return binarySearch(nums,target,0,pivot-1);
     }
 };
