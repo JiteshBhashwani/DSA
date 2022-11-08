24c6b875c23077f54daf8b2e71e88d498581e8b6
Jitesh Bhashwani <jitubhashwani999@gmail.com>
2022-11-08 10:56:22 +0530

    kadanes algo update

---
+++ Algorithms/Uncategorized/Kadane's_Algorithm.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using std::vector;
+int maxSubArray(vector<int> &nums)
+{
+    int currentSum = nums[0];
+    int maxSum = nums[0];
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (currentSum < 0)
+        {
+            currentSum = nums[i];
+        }
+        else
+        {
+            currentSum += nums[i];
+        }
+        maxSum = std::max(maxSum, currentSum); // updating max
+    }
+    return maxSum;
+}
